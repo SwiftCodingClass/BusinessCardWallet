958fc535c78728d714b3eb52e1598867a10b4654
seunggyun-jeong <jskyun98@gmail.com>
2024-09-27 23:03:56 +0900

    navigation

--- Gyunni/BusinessCardWallet_Final/BusinessCardWallet_Final/BusinessCardWallet_FinalApp.swift
+++ Gyunni/BusinessCardWallet_Final/BusinessCardWallet_Final/BusinessCardWallet_FinalApp.swift
@@ -11,7 +11,9 @@ import SwiftUI
 struct BusinessCardWallet_FinalApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            NavigationView {
+                ContentView()
+            }
         }
     }
 }
